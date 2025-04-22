5ac48ce8a6d66a3cdcbb77a10056f4ac74f52474
HANSOL <hansol302@gmail.com>
2025-04-22 22:08:20 +0900

    Setting: 프로젝트 기본 세팅

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const pretendard = localFont({
+  src: [
+    {
+      path: "../public/fonts/Pretendard-Light.otf",
+      weight: "300",
+    },
+    {
+      path: "../public/fonts/Pretendard-Regular.otf",
+      weight: "400",
+    },
+    {
+      path: "../public/fonts/Pretendard-Medium.otf",
+      weight: "500",
+    },
+    {
+      path: "../public/fonts/Pretendard-Bold.otf",
+      weight: "700",
+    },
+  ],
+});
+
+export const metadata: Metadata = {
+  title: "위버 - 상품 목록",
+  description: "상품 리스트",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${pretendard.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
