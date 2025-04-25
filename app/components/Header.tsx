1700f143e6543f6069bed5ccdff411c248275319
HANSOL <hansol302@gmail.com>
2025-04-25 16:30:26 +0900

    Style: 헤더 내 로고 추가

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,3 +1,15 @@
+import Image from "next/image";
+
 export default function Header() {
-  return <header className="w-full h-20 bg-blue-100 shadow-sm"></header>;
+  return (
+    <header className="flex items-center w-full h-20 bg-blue-100 shadow-sm">
+      <Image
+        src="https://cdn.weebur.com/assets/bi/logo.svg"
+        alt="logo"
+        width={150}
+        height={50}
+        className="pl-10"
+      />
+    </header>
+  );
 }
