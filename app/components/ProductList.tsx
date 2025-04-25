e197d1f63e60dd0f65516720c6cc3e9cc1a8366e
HANSOL <hansol302@gmail.com>
2025-04-25 15:17:57 +0900

    Style: 레이아웃 설정에 대해 토글 버튼 형식 스타일 적용

--- app/components/ProductList.tsx
+++ app/components/ProductList.tsx
@@ -112,11 +112,21 @@ export default function ProductList({
           {TOTAL_TEXT} {productsData.total}
           {PRODUCT_UNIT}
         </p>
-        <div className="flex w-fit p-2 gap-3 rounded-lg bg-gray-100">
-          <button onClick={() => changeLayoutType("list")}>
+        <div className="flex w-fit rounded-lg bg-gray-100 border-2 border-gray-100">
+          <button
+            className={`${
+              layoutType === "list" && "bg-white"
+            } p-2 rounded-l-md`}
+            onClick={() => changeLayoutType("list")}
+          >
             <Image src={listImg} width={30} height={30} alt="list" />
           </button>
-          <button onClick={() => changeLayoutType("grid")}>
+          <button
+            className={`${
+              layoutType === "grid" && "bg-white"
+            } p-2 rounded-r-md`}
+            onClick={() => changeLayoutType("grid")}
+          >
             <Image src={gridImg} width={30} height={30} alt="grid" />
           </button>
         </div>
