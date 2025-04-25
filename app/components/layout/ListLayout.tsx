fbe68201a26385c4c5865c17aa617d1129a23115
HANSOL <hansol302@gmail.com>
2025-04-25 14:00:42 +0900

    Fix: 리스트 컴포넌트에서 props 데이터를 렌더링하도록 수정

--- app/components/layout/ListLayout.tsx
+++ app/components/layout/ListLayout.tsx
@@ -1,29 +1,32 @@
+import { PRODUCT_UNIT, REVIEW_TEXT } from "@/app/constants/constants";
+import { Product } from "@/app/types/product";
 import starImg from "@/public/images/star.svg";
 import Image from "next/image";
 
-export default function ListLayout() {
+export default function ListLayout({ products }: { products: Product[] }) {
   return (
     <section>
-      <article className="flex flex-row w-full mt-5">
-        <Image src="" alt="item" width={80} height={80} />
-        <div className="flex flex-row gap-10">
-          <div>
-            <p className="text-lg font-bold">Essence Mascara Lash Princess</p>
-            <p className="text-base">
-              The Essence Mascara Lash Princess is a popular mascara known for
-              its volumizing and lengthening effects. Achieve dramatic lashes
-              with this long-lasting and cruelty-free formula.
-            </p>
-          </div>
-          <div className="flex flex-col min-w-[120px] gap-2">
-            <div className="flex flex-row gap-2">
-              <Image src={starImg} width={20} height={20} alt="star" />
-              4.94
+      {products.map((product) => (
+        <article className="flex flex-row w-full mt-6 gap-2" key={product.id}>
+          <Image src={product.thumbnail} width={150} height={150} alt="item" />
+          <div className="flex flex-row gap-10">
+            <div>
+              <p className="text-lg font-bold">{product.title}</p>
+              <p className="text-base">{product.description}</p>
             </div>
-            <p>리뷰 3개</p>
+            <div className="flex flex-col min-w-[120px] gap-2">
+              <div className="flex flex-row gap-0.5 justify-end">
+                <Image src={starImg} width={20} height={20} alt="star" />
+                {product.rating}
+              </div>
+              <p className="block text-right">
+                {REVIEW_TEXT} {product.reviews.length}
+                {PRODUCT_UNIT}
+              </p>
+            </div>
           </div>
-        </div>
-      </article>
+        </article>
+      ))}
     </section>
   );
 }
--- app/constants/constants.ts
+++ app/constants/constants.ts
@@ -7,6 +7,7 @@ export const SEARCH_BUTTON_TEXT = "검색";
 export const SEARCH_INPUT_PLACEHOLDER = "상품 검색";
 export const TOTAL_TEXT = "총";
 export const PRODUCT_UNIT = "개";
+export const REVIEW_TEXT = "리뷰";
 
 export const SORT_OPTIONS: Sort = {
   [DEFAULT_SORT]: { sortBy: "", order: "" },
