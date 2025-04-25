b9a1b57ed419635af5967e5428966b4db917302d
HANSOL <hansol302@gmail.com>
2025-04-25 15:19:48 +0900

    Fix: 데이터 길이에 따라 리스트 너비가 상이해지는 이슈 수정

--- app/components/layout/ListLayout.tsx
+++ app/components/layout/ListLayout.tsx
@@ -9,14 +9,17 @@ export default function ListLayout({ products }: { products: Product[] }) {
   return (
     <section>
       {products.map((product) => (
-        <article className="flex flex-row w-full mt-6 gap-2" key={product.id}>
+        <article
+          className="grid grid-cols-[150px_1fr_auto] w-full mt-6 gap-2"
+          key={product.id}
+        >
           <Image src={product.thumbnail} width={150} height={150} alt="item" />
           <div className="flex flex-row gap-10">
-            <div>
+            <div className="flex flex-col flex-1">
               <p className="text-lg font-bold">{product.title}</p>
               <p className="text-base">{product.description}</p>
             </div>
-            <div className="flex flex-col min-w-[120px] gap-2">
+            <div className="flex flex-col item-end min-w-[100px] gap-2">
               <div className="flex flex-row gap-0.5 justify-end">
                 <Image src={starImg} width={20} height={20} alt="star" />
                 {product.rating}
